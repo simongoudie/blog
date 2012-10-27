7fbd4527f60393ae7ccec880b7bbcb0d7b1b5ee6
Simon Goudie <simon@simongoudie.com>
2012-10-27 17:58:02 +1100

    New post script added, subfolder workaround

---
+++ _posts/2012-10-24-This-is-a-further-post-about-my-script.md
@@ -0,0 +1,6 @@
+---
+layout: post
+title: This is a further post about my script
+---
+
+Here's some details about my Jeckyll script
---
+++ newpost.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#! Script to make a new post with the date and title, add the yaml data, save it in the right folder and open it for editing in vim
+
+echo "Please enter post title:"
+read TITLE
+
+TITLEF=${TITLE// /-}
+DATE=$(date +"%Y-%m-%d")
+FILENAME=~/blog/_posts/$DATE-$TITLEF.md
+
+touch $FILENAME
+
+echo "---" >> $FILENAME
+echo "layout: post" >> $FILENAME
+echo "draft: true" >> $FILENAME
+echo "title: $TITLE" >> $FILENAME
+echo "---" >> $FILENAME
+echo "" >> $FILENAME
+echo "POST CONTENT HERE" >> $FILENAME
+
+vim $FILENAME

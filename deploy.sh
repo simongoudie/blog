0b38ff12975501d5739c8da4b9a4fd487da3e916
Simon Goudie <simon@simongoudie.com>
2013-05-06 13:56:07 +1000

    New deploy script and configs for Jekyll 1.0

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,19 @@
-#!/bin/bash
+#! /bin/bash
 
 HOST=$(hostname)
 
 if [[ $1 = -deploy ]]; then
     echo "running with simongoudie.com/blog and deploying"
     glynn
-elif [[ $1 = -deploys4 ]]; then
-    echo "running with simon4.com/blog and deploying to test"
-    jekyll && rsync -avz --delete _site/ simon@simon4.com:html/
 elif [[ $HOST = RexBook ]]; then
     echo "running with jekyll.dev"
-    jekyll --url http://jekyll.dev
+    jekyll build --config _rexbookconfig.yml
 elif [[ $HOST = simon4.com ]]; then
     echo "running with simon4.com/blog"
-    jekyll --url http://simon4.com/blog && rm -rf ~/http/blog && mkdir ~/http/blog && cp ~/blog/_site/* ~/http/blog -R
+    jekyll build --config _simon4config.yml && sudo rm -rf ~/http/blog && sudo mkdir ~/http/blog && sudo cp ~/blog/_site/* ~/http/blog -R
 elif [[ $HOST = rexpi ]]; then
     echo "running with goudz.com/blog"
-    jekyll --url http://goudz.com/blog && rm -rf ~/http/blog && mkdir ~/http/blog/ && cp ~/blog/_site/* ~/http/blog -R
+    jekyll build --config _goudzconfig.yml && sudo rm -rf ~/http/blog && sudo mkdir ~/http/blog/ && sudo cp ~/blog/_site/* ~/http/blog -R
 else
     echo "Unknown machine or switch, try something else"
 fi

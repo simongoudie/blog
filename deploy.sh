cf5f7c5cb0fd640fa9159ad5f716bb2628b3b421
Simon Goudie <simon@simongoudie.com>
2013-05-06 14:00:13 +1000

    Improved deploy and configs

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ if [[ $1 = -deploy ]]; then
     glynn
 elif [[ $HOST = RexBook ]]; then
     echo "running with jekyll.dev"
-    jekyll build --config _rexbookconfig.yml
+    jekyll build --config _config.yml,_rexbookconfig.yml
 elif [[ $HOST = simon4.com ]]; then
     echo "running with simon4.com/blog"
-    jekyll build --config _simon4config.yml && sudo rm -rf ~/http/blog && sudo mkdir ~/http/blog && sudo cp ~/blog/_site/* ~/http/blog -R
+    jekyll build --config _config.yml,_simon4config.yml && sudo rm -rf ~/http/blog && sudo mkdir ~/http/blog && sudo cp ~/blog/_site/* ~/http/blog -R
 elif [[ $HOST = rexpi ]]; then
     echo "running with goudz.com/blog"
-    jekyll build --config _goudzconfig.yml && sudo rm -rf ~/http/blog && sudo mkdir ~/http/blog/ && sudo cp ~/blog/_site/* ~/http/blog -R
+    jekyll build --config _config,_goudzconfig.yml && sudo rm -rf ~/http/blog && sudo mkdir ~/http/blog/ && sudo cp ~/blog/_site/* ~/http/blog -R
 else
     echo "Unknown machine or switch, try something else"
 fi
